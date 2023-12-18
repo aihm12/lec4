6e65a127dcf4497c2a8d1f3a5a863df7e2914850
aihm12 <152840188+aihm12@users.noreply.github.com>
2023-12-18 09:20:57 +0300

    Add files via upload

---
+++ lec1.py
@@ -0,0 +1,68 @@
+5+2
+7-2
+6/3    # real div
+6//3    # inte div 
+n=7
+msg='reeman'
+pi=3.1415
+print(msg)
+print(type(r))
+x=y=2
+m,r=3, 4
+
+
+from math import *     # import all defined function 
+print(pi)
+sqrt(4)
+t=2*pi*sqrt(1/9)
+
+#reset()
+%reset?
+reset -f
+reset -s
+
+sin(90)
+cos(30)
+tan(90)
+
+4**2
+pow(4, 2)        #show help
+
+-------------------------------------------------------
+
+a=100
+if (a>100):
+    print('yes')
+else:  
+    print('no')
+
+    
+a=100    #15
+if (a>100):
+    print('grater')
+elif (a<100):
+    print('smaller')    
+else:  
+    print('equal')
+    
+
+a=input("enter the value of a")
+a=int(a)  
+if (a%2 ==0):
+        print('even')
+else:
+        print('odd')
+         
+    
+a=int(input('Enter ineger no.'))
+if a%2==0:
+    if a>=0:
+        print('It  is positive even no.')
+    elif a<0:
+        print('It  is negative even no.')
+else:
+       if a>=0:
+        print('It  is positive odd no.')
+       elif a<0:
+        print('It  is negative odd no.')
+    
